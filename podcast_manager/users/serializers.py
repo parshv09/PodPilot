6fd706d6e50cb2ff93cff559d111ac52b8b10e45
Parshv Athane <parshvrajkumarathane@gmail.com>
2025-08-13 13:52:40 +0530

    authentication

--- podcast_manager/users/serializers.py
+++ podcast_manager/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import User
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,16 +12,20 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    """Serializer for user registration."""
-    password = serializers.CharField(write_only=True, min_length=6)
+    """Serializer for user registration with strong password validation."""
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'profile_image', 'password']
 
+    def validate_password(self, value):
+        # Use Django's built-in validators
+        validate_password(value)
+        return value
+
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
-
 
 class LoginSerializer(serializers.Serializer):
     """Serializer for user login."""
